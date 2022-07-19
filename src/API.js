0932be0142fcfcde96d0c4b6810bdfb0b768d5dd
Piyush Agarwal <piyushagarwalplay@gmail.com>
2022-07-19 17:38:35 +0530

    Weather-App-19/7/22

---
+++ src/API.js
@@ -0,0 +1,12 @@
+export const geoApiOptions  = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '19e1479c03mshc3d73c629b4e898p164e6ejsn1118812210d9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "fd1299b3f6a4c2ac2e7f26594259d9a2";
